c0c6970557ee6b4ecd890cf59697db04dd50631b
nothingtosurprise <nothingtosurprise@users.noreply.github.com>
2026-08-31 22:54:19 +0000

    deploy: 59305a20b8b439fe70d0465b0cbfb7f6786bc619

---
+++ assets/chunks/okta-authorization-server-rule.CrqafiSe.js
@@ -0,0 +1,1 @@
+const s="/screenshots/sso/okta-create-app-integration.png",o="/screenshots/sso/okta-groups-claim.png",t="/screenshots/sso/okta-authorization-server-rule.png";export{s as _,o as a,t as b};
